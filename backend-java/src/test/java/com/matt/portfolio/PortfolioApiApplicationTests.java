7a3d05202db48e58548f901338af895350e2f824
mattDev0 <amatthew642@gmail.com>
2026-06-11 13:08:15 +0100

    test(java): enhance context load test with bean checks

--- backend-java/src/test/java/com/matt/portfolio/PortfolioApiApplicationTests.java
+++ backend-java/src/test/java/com/matt/portfolio/PortfolioApiApplicationTests.java
@@ -1,13 +1,24 @@
 package com.matt.portfolio;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PortfolioApiApplicationTests {
 
+	@Autowired
+	private GitHubController gitHubController;
+
+	@Autowired
+	private InfrastructureController infrastructureController;
+
 	@Test
 	void contextLoads() {
+		assertThat(gitHubController).isNotNull();
+		assertThat(infrastructureController).isNotNull();
 	}
 
 }
